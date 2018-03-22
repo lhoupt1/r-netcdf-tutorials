9b7f2c7889f5eca6e90bfc125a5fb76d1e547705
Konrad Hafen <konrad.hafen@gmail.com>
2018-03-22 08:33:11 -0700

    code and file switch

--- netcdf_get_data.R
+++ netcdf_get_data.R
@@ -6,3 +6,23 @@ install.packages("ncdf4")
 
 #load ndcf4 package
 library(ncdf4)
+
+#open netcdf file
+#file.choose() allows user to choose file from system
+nc <- nc_open(file.choose())
+
+#print metadata for netcdf file
+print(nc)
+
+#show the data attributes for the file
+attributes(nc$var)
+
+#show the names of the data attributes
+attributes(nc$var)$names
+
+#get the name of a specific var by index
+attributes(nc$var)$names[1]
+
+#get a variable's data
+dat <- ncvar_get(nc, attributes(nc$var)$names[1])
+

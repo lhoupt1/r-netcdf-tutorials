f0d428643fec43d45495474f819f8265fcc20a95
Konrad Hafen <konrad.hafen@gmail.com>
2018-03-21 17:12:00 -0700

    first commit

---
+++ netcdf_get_data.R
@@ -0,0 +1,8 @@
+rm(list=ls())
+
+#install netcdf4 if not alread installed
+#you will need to install netcdf4 and hdf5 (not R packages)
+install.packages("ncdf4")
+
+#load ndcf4 package
+library(ncdf4)
